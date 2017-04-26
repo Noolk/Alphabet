6f26471df682b46b7ec96fd61ee4572b2b6be00d
Светлана <svetlana_koloskova99@mail.ru>
2017-04-26 23:23:06 +0700

    Added struct

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <string.h>
 
+struct words
+{
+     char word[10]; 
+};
+
 int main()
 {
    setlocale(LC_ALL, "RUS");
    int n = 0;
    printf ("Введите количество записей: ");
    scanf("%d", &n);
+   struct words A[n];
 }
+
+
