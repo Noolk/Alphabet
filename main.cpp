482a9105b8a55db0882e693b46d53c8322c9dabd
Noolkmvm <noolkmvm@gmail.com>
2017-04-26 23:27:27 +0700

    Added filling of the file

--- main.cpp
+++ main.cpp
@@ -10,9 +10,18 @@ int main()
 {
    setlocale(LC_ALL, "RUS");
    int n = 0;
+   FILE *bf1;
    printf ("Введите количество записей: ");
    scanf("%d", &n);
    struct words A[n];
+   bf1 = fopen ("source.txt", "w");
+   printf ("\nСлова: ");
+    for (i = 0; i < n; i++)
+     {
+       scanf ("%s", A[i].word);
+       fwrite (&A[i], sizeof(A), 1, bf1);
+     }  
+    fclose (bf1);
 }
 
 
