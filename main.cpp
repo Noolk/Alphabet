97ec2de64b049a5568ba08dfbc6e722aabfc497b
Noolkmvm <noolkmvm@gmail.com>
2017-04-26 23:50:49 +0700

    Added output of sorted words

--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@ struct words
 void InsertSort(char A[][10],int n);
 
 int main()
-{
-   setlocale(LC_ALL, "RUS");
-   int n = 0, i;
-   FILE *bf1;
-   printf ("Введите количество записей: ");
-   scanf("%d", &n);
-   struct words A[n];
-   bf1 = fopen ("source.txt", "w");
-   printf ("\nСлова: ");
+{	
+    setlocale(LC_ALL, "RUS");
+    FILE *bf1, *bf2;
+    int n = 0, i;
+    printf ("Введите количество записей: ");
+    scanf("%d", &n);
+    struct words A[n];
+    bf1 = fopen ("source.txt", "w");
+    printf ("\nСлова: ");
     for (i = 0; i < n; i++)
      {
        scanf ("%s", A[i].word);
@@ -36,16 +36,25 @@ int main()
     char B[n][10];      
     for (i=0;i<n;i++)
     strcpy(B[i],A[i].word);
-    printf ("\n");
-    InsertSort(B,n);
+    printf ("\n");	
+    InsertSort(B,n);      
     bf2 = fopen ("sorted.txt", "w");
     for (i = 0; i < n; i++)
      {
        fwrite (&B[i], sizeof(B), 1, bf2);
      }
     fclose(bf2);
+    bf2 = fopen ("sorted.txt", "r");
+    printf ("Отсортированные слова: \n\n");
+    for (i = 0; i < n; i++)
+     {
+       fread (&A[i], sizeof(A), 1, bf2);
+       if (A[i].word != 0)
+       printf ("%s\n", A[i].word);
+     }
+    fclose(bf2);     
 }
-
+		
 void InsertSort(char A[][10], int n) 
 {
 	int j, i; char t[10];
@@ -61,5 +70,7 @@ void InsertSort(char A[][10], int n)
 	strcpy(A[j+1],t);	
 	 }
 }
-
-
+	
+	
+	
+	
